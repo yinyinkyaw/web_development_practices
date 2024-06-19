b8aaef2c3220706a8cdc1a7706c8657176b00b41
Yin Yin Kyaw <yinyinkyaw13@gmail.com>
2024-06-19 17:41:38 +0630

    [implement] click on box selected

--- interactive_shape_project/src/App.tsx
+++ interactive_shape_project/src/App.tsx
@@ -8,17 +8,27 @@ function App() {
   const [clickPoints, setClickPoints] = useState<Array<Array<number>>>(BOX);
   // const [clickOrders, setClickOrders] = useState();
 
-  const handleClick = (point: Array<number>) => {};
+  const handleClick = (point: Array<number>) => {
+    const modifyClickPoints = clickPoints?.map((row, rowId) =>
+      rowId === point[0]
+        ? row.map((col, colIndex) => (colIndex === point[1] ? 1 : col))
+        : row
+    );
 
-  console.log("clicked points::", clickPoints);
+    setClickPoints(modifyClickPoints);
+  };
+
   return (
     <div className="box_container">
       {BOX.map((row, rowIndex) =>
         row.map((_, colIndex) => (
           <button
+            disabled={!!clickPoints[rowIndex][colIndex]}
             className={`box ${clickPoints[rowIndex][colIndex] && "active"}`}
             onClick={() => handleClick([rowIndex, colIndex])}
-          />
+          >
+            {rowIndex} x {colIndex}
+          </button>
         ))
       )}
     </div>
