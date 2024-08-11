5812542f65ab4790e9f35898e3fec27eed6408d0
Yin Yin Kyaw <yinyinkyaw13@gmail.com>
2024-08-11 21:44:01 +0630

    [implement] click theme button for dark theme

--- frontend_quiz/src/tests/components/Theme/ThemeSwitcher.test.tsx
+++ frontend_quiz/src/tests/components/Theme/ThemeSwitcher.test.tsx
@@ -2,6 +2,7 @@ import { it, describe, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import ThemeSwitcher from "@/components/ThemeSwitcher/ThemeSwitcher.component";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
 describe("Theme Switcher", () => {
   it("initial theme should be light", () => {
@@ -9,8 +10,21 @@ describe("Theme Switcher", () => {
 
     const checkBox = screen.queryByRole("checkbox");
 
-    expect(checkBox).toBeTruthy();
+    expect(checkBox).toBeChecked();
     expect(screen.getByTitle("sun-light")).toBeInTheDocument();
     expect(screen.getByTitle("moon-light")).toBeInTheDocument();
+  });
+
+  it("click on theme toggle then theme should be dark", async () => {
+    const user = userEvent.setup();
+
+    render(<ThemeSwitcher />);
+
+    const checkBox = screen.getByRole("checkbox");
+    await user.click(checkBox);
+
+    expect(checkBox).not.toBeChecked();
+    expect(screen.getByTitle("sun-dark")).toBeInTheDocument();
+    expect(screen.getByTitle("moon-dark")).toBeInTheDocument();
   });
 });
